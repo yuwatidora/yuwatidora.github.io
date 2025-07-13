0a91fcca3655244a29e7fc1ef8fa170e3e015fd8
yuwatidora <yuwatinyi003@gmail.com>
2025-07-13 16:35:27 -0400

    revert: remove github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/portfolio",
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
