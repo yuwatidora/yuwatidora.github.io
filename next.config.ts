db34166fb844bca86f133eb86e9344a65450b51e
yuwatidora <yuwatinyi003@gmail.com>
2025-07-13 17:35:23 -0400

    Undo GitHub Pages deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
+  /* config options here */
 };
 
 export default nextConfig;
