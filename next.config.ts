5a2000ace5c15c5f1e58c527620a279967f1da95
yuwatidora <yuwatinyi003@gmail.com>
2025-07-13 23:07:32 -0400

    Remove basePath for main GitHub Pages site

--- next.config.ts
+++ next.config.ts
@@ -9,13 +9,6 @@ const nextConfig: NextConfig = {
   output: "export",
 
   /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/portfolio",
-
-  /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
    *
